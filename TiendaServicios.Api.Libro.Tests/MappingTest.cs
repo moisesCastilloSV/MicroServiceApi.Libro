be8b96c0320e3bbb096bac9ecf401a49a2ed034b
MoisesCastillo <moisesudb17@gmail.com>
2022-04-18 00:09:20 -0600

    Soporte Pruebas Unitarias
    Unit test

---
+++ TiendaServicios.Api.Libro.Tests/MappingTest.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Dto;
+using TiendaServicios.Api.Libro.Model;
+
+namespace TiendaServicios.Api.Libro.Tests
+{
+    public class MappingTest : Profile
+    {
+        public MappingTest()
+        {
+            CreateMap<LibreriaMaterial, LibreriaMaterialDto>();
+        }
+
+    }
+}
